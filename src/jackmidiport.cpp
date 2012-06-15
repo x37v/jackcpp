2d32d7689a811ae2649a18da8fee0df078c44e00
Alex Norman <x37v.alex@gmail.com>
2012-06-14 23:18:14 -0700

    gpl header

--- src/jackmidiport.cpp
+++ src/jackmidiport.cpp
@@ -1,3 +1,21 @@
+//C++ Classes that wrap JACK
+//Copyright 2007, 2012 Alex Norman
+//
+//This file is part of JACKC++.
+//
+//JACKC++ is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//JACKC++ is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with JACKC++.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "jackmidiport.hpp"
 
 JackCpp::MIDIPort::MIDIPort() : mPort(NULL) {
@@ -15,3 +33,4 @@ void JackCpp::MIDIPort::init(AudioIO * audio_client, std::string name, port_t ty
 std::string JackCpp::MIDIPort::name() const {
    return std::string(jack_port_name(mPort));
 }
+
